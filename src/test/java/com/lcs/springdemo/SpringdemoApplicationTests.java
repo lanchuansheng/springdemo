3d8cbe9ab97afbf4a6dbb29cac7d1acafb4e851d
lancs <570297931@qq.com>
2020-03-03 22:40:48 +0800

    111

---
+++ src/test/java/com/lcs/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcs.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
