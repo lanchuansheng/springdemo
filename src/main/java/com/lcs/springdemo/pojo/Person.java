c0a436814872aea011b3ee541808944686cce836
lancs <570297931@qq.com>
2020-03-05 23:12:00 +0800

    111

---
+++ src/main/java/com/lcs/springdemo/pojo/Person.java
@@ -0,0 +1,50 @@
+package com.lcs.springdemo.pojo;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+//通过yml配置绑定属性
+@ConfigurationProperties(prefix = "person")
+//通过配置文件绑定属性（不推荐）
+//@PropertySource(value = "classpath:lan.txt")
+public class Person {
+
+    private String name;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/test/java/com/lcs/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/lcs/springdemo/SpringdemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.lcs.springdemo;
 
+import com.lcs.springdemo.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SpringdemoApplicationTests {
 
+    @Autowired
+    private Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
